5f36c4799432fec875bcac242b2f044d5eb61a50
inforussle <123Jimmyn@gmail.com>
2022-11-23 12:21:17 -0500

    Prettying the index format, removing web/ folder as it is no longer used

--- web_creator.py
+++ web_creator.py
@@ -10,24 +10,26 @@ def index_files(folder_path:str) -> list:
 
 def folder_to_links(folder_path:str) -> list:
     files = index_files(folder_path)
-    files.sort()
-    links = ['<ul>', '</ul>']
+    links = []
     for file in files:
         link = f'<li><a href="./Formatted/{file}.txt" target="_self">{file}</a></li>'
-        links.insert(len(links)-1, link)
+        links.append(link)
     return links
 
 def create_index(folder_path:str) -> None:
     content = ['<html>','<head>','\t<title>Recipe Book</title>','</head>','<style>',
-                '\ta:link, a:visited { color: blue }','</style>','<body>','\t<h4>My Collected Recipies</h4>',
-                '\t<hr>','<!--Begin Recipies-->','','<!--End Recipes-->','</body>','</html>']
+                '\ta:link, a:visited { color: blue }', '\tul { list-style-type: none}',
+                '\tdiv { max-width: 40vw; margin: auto }','\th3 { text-align: center }',
+                '</style>','<body>','\t<div>','\t\t<h4>My Collected Recipies</h4>','\t\t<hr>',
+                '\t\t<ul>','<!--Begin Recipes-->','','<!--End Recipes-->','\t\t</ul>','\t</div>',
+                '</body>','</html>']
     content = '\n'.join(content)
     with open('index.html', 'w') as f:
         f.write(content)
 
 def update_index(folder_path:str) -> None:
     links = '\n'.join([l for l in folder_to_links(folder_path)])
-    matching_string = r'(?<=<!--Begin Recipies-->\s)[\S\s]*(?=\s<!--End Recipies-->)'
+    matching_string = r'(?<=<!--Begin Recipes-->\s)[\S\s]*(?=\s<!--End Recipes-->)'
     file_contents = ''
 
     if not os.path.isfile('index.html'):
